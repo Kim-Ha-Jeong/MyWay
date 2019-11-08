4e01f43ec5d347b67459bb3915e9ce12db8e3064
Kim-Ha-Jeong <kha031821@gmail.com>
2019-11-08 15:07:00 +0900

    기본 틀 구성

---
+++ app.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+/*
+var client = mysql.createConnection({
+  user: 'root',
+  password: 'pearflower2019',
+  database: 'pearflower'
+});
+*/
+
+/*여기부터 새로 작성*/
+
+
+var client = mysql.createConnection({
+    host: "pearflower2019.cbyhsdhnditm.ap-northeast-2.rds.amazonaws.com",
+    user: "root",
+    database: "pearflower",
+    password: "pearflower2019",
+    port: 3306
+});
+
+
+/*여기까지 새로 작성*/
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+
+app.use(express.static('public'));
+app.use(express.static('css'));
+app.use(express.static('image'));
+app.use(express.static('js'));
+
+
+//호스팅 할 때는 포트넘버 80 고정
+app.listen(3000, function () {
+  console.log('server running at localhost:1886');
+});
+
+//홈
+app.get('/', function (request, response) {
+  fs.readFile('home.html', 'utf8', function (error, data) {
+      response.send(data);
+  });
+});
+
