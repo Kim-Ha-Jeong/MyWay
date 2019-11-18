a6ab2c233ef36980e542b0ce2b3b40a64775ff96
Kim-Ha-Jeong <kha031821@gmail.com>
2019-11-19 01:11:44 +0900

    gitignore 파일 만듦

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ var fs = require('fs');
 var ejs = require('ejs');
 var express = require('express');
 var bodyParser = require('body-parser');
-/*
-var client = mysql.createConnection({
-  user: 'root',
-  password: 'pearflower2019',
-  database: 'pearflower'
-  port : 3306
-});
-*/
+var mysql = require('mysql');
 
+var db = mysql.createConnection({
+  host : "http://localhost:3000",
+  user: 'root',
+  port : 3306,
+  password: '111111',
+  database: 'MyWay'
+});
+db.connect();
+
+app.get('/insert', function (request, response) {
+  fs.readFile('noticeInsert.html', 'utf8', function (error, data) {
+    response.send(data);
+  });
+});
+
+app.post('/noticeManager/insert', function (request, response) {
+  var body = request.body;
+  client.query('INSERT INTO postlist (num, title, post) VALUES (?, ?, ?)', [
+      body.num, body.title, body.post
+  ], function () {
+    response.redirect('/noticeManager');
+  });
+});
 
 /*여기까지 새로 작성*/
 var app = express();
@@ -19,14 +35,12 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-
 app.use(express.static('public'));
 app.use(express.static('css'));
 app.use(express.static('image'));
 app.use(express.static('js'));
 
 
-//호스팅 할 때는 포트넘버 80 고정
 app.listen(3000, function () {
   console.log('server running at localhost:3000');
 });
@@ -44,8 +58,8 @@ app.get('/picture', function (request, response) {
   });
 });
 
-app.get('/demo', function (request, response) {
-  fs.readFile('demo.html', 'utf8', function (error, data) {
+app.get('/sign', function (request, response) {
+  fs.readFile('sign.html', 'utf8', function (error, data) {
       response.send(data);
   });
 });
