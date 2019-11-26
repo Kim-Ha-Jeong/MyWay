777650e99b4cdb4b903e7594c00a7914372ae44b
suzie4831 <suzie4831@naver.com>
2019-11-26 12:03:21 +0900

    데이터 엑셀 저장 도전

--- line5.py
+++ line5.py
@@ -5,21 +5,39 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import datetime
 
 driver=webdriver.Chrome('C:/Users/suzie/Downloads/chromedriver_win32/chromedriver.exe')     #크롬드라이버 깔고 chromedriver.exe위치 이것처럼 써넣어야함
+
+dt = datetime.datetime.now()
+filename = '지하철' + dt.strftime("%Y_%m_%d")
+f = open(filename + '.csv', 'w')
+
+station_name_final=[]
+facil_list_final=[]
+
 for i in range(11,49):
     url='https://place.map.kakao.com/SES25'+str(i)
     driver.get(url)
     time.sleep(3)
 
+    row=0
     station = driver.find_elements_by_css_selector(".tit_station")
-    station_name=station[0].text.split('선')
+    station_name=station[0].text.split('선',1)
 
     facil_list=driver.find_element_by_class_name("list_sfacility").find_elements_by_tag_name("li")
     
-    for i in range(len(station_name)-1,len(station_name)):
+    for i in range(1,2):
         print('<역이름: ',station_name[i],'>','\n')
+        station_name_final.append(station_name[i])
   
     for facil in facil_list:
         print(facil.text+'\n\n')
+        facil_list_final.append(facil.text)
+
+
+    
+    
+
+    
     
