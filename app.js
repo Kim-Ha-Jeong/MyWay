e1df9e7592b94330a41b182b583748db67c11f6b
suzie4831 <suzie4831@naver.com>
2019-11-30 02:28:21 +0900

    테스트중

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ app.post('/dropOut', function(request,response,next){
   });
 })
 
-app.get('/B01', function (request, response) {
+app.get('/138', function (request, response) {
   fs.readFile('suwon.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
       response.send(ejs.render(data, {
