3a41c956d41d7952a968c2b5868e783c7b7d656e
suzie4831 <suzie4831@naver.com>
2019-12-03 17:36:30 +0900

    검색 구현중

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var router = express.Router();
 var db = mysql.createConnection({
   user: 'root',
   port : 3306,
-  password: '111111',
+  password: '1234',
   database: 'MyWay'
 });
 db.connect();
@@ -238,6 +238,22 @@ app.get('/board', function (request, response) {
         data: results
       }));
     });
+  });
+});
+
+app.post('/board', function(request, response){
+  var search = request.body['search'];
+  db.query('select * from board where title like ? or description like ?',["%"+search+"%","%"+search+"%"], function (err, rows, fields) {
+      if (!err) {
+          if (rows[0]!=undefined) {
+              response.redirect("/board#"+rows[0]['num']);
+          } else {
+              response.send('no data');
+          }
+
+      } else {
+          response.send('error : ' + err);
+      }
   });
 });
 
