2aca39770ae1075fd6cce62337290b5a00b0b404
suzie4831 <suzie4831@naver.com>
2019-12-03 15:34:21 +0900

    충돌해결

--- app.js
+++ app.js
@@ -146,21 +146,8 @@ app.get('/138', function (request, response) {
   });
 });
 
-app.post('/138', function(request, response){
-  var stationName = request.body['stationName'];
-  db.query('select * from station where 역이름=?',[stationName], function (err, rows, fields) {
-      if (!err) {
-          if (rows[0]!=undefined) {
-              response.redirect("/"+rows[0]['num']);
-
-          } else {
-              response.send('no data');
-          }
-
-      } else {
-          response.send('error : ' + err);
-      }
-  });
+app.post('/138', function (request, response) {
+  
 });
 
 
@@ -224,11 +211,6 @@ app.get('/hashtag', function (request, response) {
   });
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 74932a86911d4c17c5393577f5e4c62bebf49037
 app.get('/information', function (request, response) {
   fs.readFile('information.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
@@ -236,21 +218,5 @@ app.get('/information', function (request, response) {
         data: results
       }));
     });
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 74932a86911d4c17c5393577f5e4c62bebf49037
-app.post('/hashtag', function (request, response) {
-  var tagName = request.body['tagName'];
-  db.query('INSERT INTO tag (title) VALUES (?)', [
-      tagName
-  ], function () {
-    response.redirect('/hashtag');
-<<<<<<< HEAD
->>>>>>> 364f8b0ae6fdf9ffa0699ac54442307c34a31048
-=======
-
->>>>>>> 74932a86911d4c17c5393577f5e4c62bebf49037
   });
 });
