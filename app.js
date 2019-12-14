5739ae316c8e632cf79457157a44ad27289371bf
Kim-Ha-Jeong <kha031821@gmail.com>
2019-12-14 16:30:51 +0900

    app.js 코드 정리

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* 기본 */
 var fs = require('fs');
 var ejs = require('ejs');
 var express = require('express');
@@ -6,7 +7,6 @@ const md5 = require('md5');
 var router = express.Router();
 
 /* mysql 연결 */
-
 var mysql = require('mysql');
 var db = mysql.createConnection({
     user: 'root',
@@ -21,13 +21,13 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-
+/* 이미지, css, js 연결 */
 app.use(express.static('public'));
 app.use(express.static('css'));
 app.use(express.static('image'));
 app.use(express.static('js'));
 
-
+/* localhost:3000 */
 app.listen(3000, function () {
   console.log('server running at localhost:3000');
 });
@@ -46,6 +46,7 @@ app.get('/', function (request, response) {
   });
 });
 
+/* 역검색 */
 app.post('/', function (request, response) {
   var stationName = request.body['stationName'];
   db.query('select * from station where 역이름=?',[stationName], function (err, rows, fields) {
@@ -108,7 +109,8 @@ app.post('/login', function (request, response) {
       }
   });
 });
-/*마이페이지*/
+
+/* 마이페이지 */
 app.get('/user', function (request, response) {
   fs.readFile('user.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM sign where id=? and password=?',[userId,userPw], function (error, results) {
@@ -119,6 +121,7 @@ app.get('/user', function (request, response) {
   });
 });
 
+/* 로그인 후 화면 */
 app.get('/homeLogin', function (request, response) {
   fs.readFile('homeLogin.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM board', function (error, results) {
@@ -152,6 +155,33 @@ app.post('/homeLogin', function (request, response) {
 });
 
 
+/* 회원 정보 수정 */
+app.get('/signInfo', function (request, response) {
+  fs.readFile('signInfo.html', 'utf8', function (error, data) {
+    db.query('SELECT * FROM sign where id=? and password=?',[userId,userPw], function (error, results) {
+      response.send(ejs.render(data, {
+        data: results
+      }));
+    });
+  });
+});
+
+app.post('/signInfo', function (request, response) {
+  var id = request.body['id'];
+  var password = request.body['password'];
+  var email = request.body['email'];
+  var name = request.body['name'];
+  var phone = request.body['phone'];
+  fs.readFile('signInfo.html', 'utf8', function (error, data) {
+    db.query('update sign set password=?,email=?,name=?,phone=?,secret=? WHERE id = ?', [
+        md5(password),email,name,phone,password,id
+    ], function (error, result) {
+      response.redirect("/");
+    });
+  });
+});
+
+/* 회원탈퇴 */
 app.get('/dropOut', function (request, response) {
   fs.readFile('dropOut.html', 'utf8', function (error, data) {
     response.send(data);
@@ -179,17 +209,76 @@ app.post('/dropOut', function(request,response,next){
   });
 })
 
-/*
-app.get('/138H', function (request, response) {
-  fs.readFile('suwon.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
+/* 게시판 화면*/
+/* 게시판 */
+app.post('/board', function (request, response) {
+  fs.readFile('board.html', 'utf8', function (error, data) {
+    db.query('SELECT * FROM board', function (error, results) {
       response.send(ejs.render(data, {
         data: results
       }));
     });
   });
 });
-*/
+
+app.get('/board', function (request, response) {
+  fs.readFile('board.html', 'utf8', function (error, data) {
+    db.query('SELECT * FROM board', function (error, results) {
+      response.send(ejs.render(data, {
+        data: results
+      }));
+    });
+  });
+});
+
+/* 게시판 mysql에 좋아요 +1 */
+app.get('/board/edit/:id', function (request, response) {
+  fs.readFile('board.html', 'utf8', function (error, data) {
+    db.query('update board set love=love+1 WHERE num = ?', [
+        request.params.id
+    ], function (error, result) {
+      response.redirect("/board#"+(1+request.params.id*1));
+    });
+  });
+});
+
+/* 게시판 글쓰기 */
+app.get('/insert', function (request, response) {
+  fs.readFile('boardInsert.html', 'utf8', function (error, data) {
+    response.send(data);
+  });
+});
+
+app.post('/insert', function (request, response) {
+  var body = request.body;
+  db.query('INSERT INTO board (title, description, love,type) VALUES (?, ?,0, ?)', [
+      body.title, body.description, body.type
+  ], function () {
+    response.redirect('/board');
+  });
+});
+
+/* 좋아요 순으로 정렬 */
+app.get('/board/like', function (request, response) {
+  fs.readFile('board.html', 'utf8', function (error, data) {
+    db.query('SELECT * FROM board order by love asc', function (error, results) {
+      response.send(ejs.render(data, {
+        data: results
+      }));
+    });
+  });
+});
+
+app.post('/board', function (request, response) {
+  fs.readFile('board.html', 'utf8', function (error, data) {
+    db.query(' select * from board order by love DESC;', function (error, result) {
+      response.redirect("/board/like");
+    });
+  });
+});
+
+/* 상세보기 */
+/* 수원역 해시태그 */
 app.post('/138H', function (request, response) {
   var tagName = request.body['tagName'];
   db.query('INSERT INTO tag (title) VALUES (?)', [
@@ -199,6 +288,7 @@ app.post('/138H', function (request, response) {
   });
 });
 
+/* 수원역 상세보기 */
 app.get('/138', function (request, response) {
   fs.readFile('suwon.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
@@ -226,28 +316,7 @@ app.post('/138', function (request, response) {
   });
 });
 
-
-/* 게시판 화면*/
-app.get('/board', function (request, response) {
-  fs.readFile('board.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM board', function (error, results) {
-      response.send(ejs.render(data, {
-        data: results
-      }));
-    });
-  });
-});
-
-app.get('/board/edit/:id', function (request, response) {
-  fs.readFile('board.html', 'utf8', function (error, data) {
-    db.query('update board set love=love+1 WHERE num = ?', [
-        request.params.id
-    ], function (error, result) {
-      response.redirect("/board#"+(1+request.params.id*1));
-    });
-  });
-});
-
+/* 상세보기 첫번째 div 게시판 */
 app.get('/boardS', function (request, response) {
   fs.readFile('boardS.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM board', function (error, results) {
@@ -258,21 +327,7 @@ app.get('/boardS', function (request, response) {
   });
 });
 
-app.get('/insert', function (request, response) {
-  fs.readFile('boardInsert.html', 'utf8', function (error, data) {
-    response.send(data);
-  });
-});
-
-app.post('/insert', function (request, response) {
-  var body = request.body;
-  db.query('INSERT INTO board (title, description, love,type) VALUES (?, ?,0, ?)', [
-      body.title, body.description, body.type
-  ], function () {
-    response.redirect('/board');
-  });
-});
-
+/* 상세보기 두번째 div 해시태그 */
 app.get('/hashtag', function (request, response) {
   fs.readFile('hashtag.html', 'utf8', function (error, data) {
     db.query('SELECT * FROM tag', function (error, results) {
@@ -282,18 +337,6 @@ app.get('/hashtag', function (request, response) {
     });
   });
 });
-
-app.get('/information', function (request, response) {
-  fs.readFile('information.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
-      response.send(ejs.render(data, {
-        data: results
-      }));
-    });
-  });
-});
-
-
 app.post('/hashtag', function (request, response) {
   var tagName = request.body['tagName'];
   db.query('INSERT INTO tag (title) VALUES (?)', [
@@ -304,54 +347,10 @@ app.post('/hashtag', function (request, response) {
     });
   });
 
-
-app.get('/signInfo', function (request, response) {
-  fs.readFile('signInfo.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM sign where id=? and password=?',[userId,userPw], function (error, results) {
-      response.send(ejs.render(data, {
-        data: results
-      }));
-    });
-  });
-});
-
-app.post('/signInfo', function (request, response) {
-  var id = request.body['id'];
-  var password = request.body['password'];
-  var email = request.body['email'];
-  var name = request.body['name'];
-  var phone = request.body['phone'];
-  fs.readFile('signInfo.html', 'utf8', function (error, data) {
-    db.query('update sign set password=?,email=?,name=?,phone=?,secret=? WHERE id = ?', [
-        md5(password),email,name,phone,password,id
-    ], function (error, result) {
-      response.redirect("/");
-    });
-  });
-});
-
-app.post('/board', function (request, response) {
-  fs.readFile('board.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM board', function (error, results) {
-      response.send(ejs.render(data, {
-        data: results
-      }));
-    });
-  });
-});
-
-app.post('/board', function (request, response) {
-  fs.readFile('board.html', 'utf8', function (error, data) {
-    db.query(' select * from board order by love DESC;', function (error, result) {
-      response.redirect("/board");
-    });
-  });
-});
-
-
-app.get('/board/like', function (request, response) {
-  fs.readFile('board.html', 'utf8', function (error, data) {
-    db.query('SELECT * FROM board order by love asc', function (error, results) {
+/* 상세보기 세번째 div 역정보 */
+app.get('/information', function (request, response) {
+  fs.readFile('information.html', 'utf8', function (error, data) {
+    db.query('SELECT * FROM station where 역이름="수원" and 선="1"', function (error, results) {
       response.send(ejs.render(data, {
         data: results
       }));
