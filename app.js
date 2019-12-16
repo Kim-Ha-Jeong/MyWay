3d63012f4bcddde64a229df76feec33b0f8765fc
Kim-Ha-Jeong <kha031821@gmail.com>
2019-12-16 15:02:45 +0900

    색변경

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     user: 'root',
     port : 3306,
-    password: '1234',
+    password: '111111',
     database: 'MyWay'
   });
   db.connect();
